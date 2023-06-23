85fced78a115be23ad6b8769bf8497f56a3791e1
Michael Vistocci <mikevist68@yahoo.com>
2023-06-23 14:50:04 -0400

    UpdateProductView

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -7,5 +7,12 @@ namespace Testing
     {
         public IEnumerable<Product> GetAllProducts();
         public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
+
+        
+        
+
+        
+
     }
 }
