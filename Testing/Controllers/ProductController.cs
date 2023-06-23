a09130e95f373fc16751bbaa35391cae45042998
Michael Vistocci <mikevist68@yahoo.com>
2023-06-23 15:31:12 -0400

    deleteproductview

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -57,8 +57,12 @@ namespace Testing.Controllers
             repo.InsertProduct(productToInsert);
             return RedirectToAction("Index");
         }
-    
-    
-    
+
+        public IActionResult DeleteProduct(Product product)
+        {
+            repo.DeleteProduct(product);
+            return RedirectToAction("Index");
+        }
+
     }
 }
